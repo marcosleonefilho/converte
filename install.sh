25dec14b51ab58868067fad158517a0835ef0915
Marcos Leone Filho <marcosleonefilho@gmail.com>
2013-03-29 15:40:56 -0400

    bug fixes

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo apt-get install menconder mplayer2
+sudo apt-get install mencoder mplayer2
 
 #setting gnome to show files from desktop folder on dashboard
 gsettings set org.gnome.desktop.background show-desktop-icons true
 
-source ${HOME}/.config/user-dirs.dirs
+source ~/.config/user-dirs.dirs
 
 #installing app:
-mkdir ${XDG_DESKTOP_DIR}/origem
-mkdir ${XDG_DESKTOP_DIR}/destino
-cp ${HOME}/converte/converte.desktop ${XDG_DESKTOP_DIR}/
+mkdir -p ${XDG_DESKTOP_DIR}/origem
+mkdir -p ${XDG_DESKTOP_DIR}/destino
+cp ~/converte/converte.desktop ${XDG_DESKTOP_DIR}/
