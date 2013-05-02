2047f5d3c7649681b49ec3cac0b21a3b6d729bab
Marcos Leone Filho <marcosleonefilho@gmail.com>
2013-05-02 18:12:30 -0400

    more fixes

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ gsettings set org.gnome.desktop.background show-desktop-icons true
 source ~/.config/user-dirs.dirs
 
 #installing app:
-mkdir -p ${XDG_DESKTOP_DIR}/origem
-mkdir -p ${XDG_DESKTOP_DIR}/destino
+mkdir -p "${XDG_DESKTOP_DIR}/origem"
+mkdir -p "${XDG_DESKTOP_DIR}/destino"
 echo instalando aplicativo na area de trabalho ${XDG_DESKTOP_DIR}
 cp ~/converte/converte.desktop "${XDG_DESKTOP_DIR}/"
 echo aplicativo instalado com sucesso. 
